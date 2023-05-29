c1202770d4134be41aafc45ac177ab276af04462
Luis Alberto Jaime B <39748335+luisJaimeB@users.noreply.github.com>
2023-05-29 17:15:30 -0500

    Feat: Filters have been added to avoid duplication of assets and credit type.

--- leerArchivo.php
+++ leerArchivo.php
@@ -1,68 +1,73 @@
 <?php
 $nombre_archivo = $_FILES['archivo']['name'];
-$tipo_archivo = $_FILES['archivo']['type'];
 $tamano_archivo = $_FILES['archivo']['size'];
 $ubicacion = 'archivos_csv/' . $nombre_archivo;
-$fila = 0;
-$ranges = new stdClass();
 $ranges = [];
-$credits = new stdClass();
 $credits = [];
-$includeBines = [];
-$includCredits = [];
-$include = [];
 
 if (move_uploaded_file($_FILES['archivo']['tmp_name'], $ubicacion)) {
-    //Procesar archivo CSV y mostrar datos en la tabla
-    //var_dump($_FILES);
-    $file = fopen("$ubicacion", 'r');
+    // Procesar archivo CSV y mostrar datos en la tabla
+    $file = fopen($ubicacion, 'r');
+
+    // Ignorar la primera línea
+    fgetcsv($file, 1000, ';');
 
     while (($datos = fgetcsv($file, 1000, ';')) !== false) {
-        $numero = count($datos);
-        // Saltar la primera línea
-        if ($fila == 0) {
-            $fila++;
-            continue;
+        $todos = explode(',', $datos[2]);
+
+        foreach ($todos as $valor) {
+            // agrega un cero a la izquierda
+            $numero_pad = str_pad($valor, 2, '0', STR_PAD_LEFT);
+            // toma los dos primeros digitos
+            $cuotas_int = substr(strval($numero_pad), 0, 2);
+            
+
+            // Verificar si el crédito ya existe en el arreglo
+            $existingCredit = null;
+            foreach ($credits as $credit) {
+                if ($credit['code'] === $valor) {
+                    $existingCredit = $credit;
+                    break;
+                }
+            }
+
+            // Si el crédito no existe, agregarlo al arreglo
+            if (!$existingCredit) {
+                $credit = [
+                    'description' => 'PLAN 0% ' . $valor . ' CUOTAS',
+                    'code' => $valor,
+                    'installment' => $cuotas_int,
+                    'terminalNumber' => $datos[0],
+                    'merchantCode' => $datos[1],
+                ];
+
+                $credits[] = $credit;
+            }
         }
 
-        // incluye la lista de bines
-        $includeBines = [
+        $range = [
             'bin' => $datos[3],
             'start' => $datos[4],
             'end' => $datos[5],
         ];
 
-        //separa los tipos de creditos
-        $todos = explode(',', $datos[2]);
-
-        foreach ($todos as $valor) {
-            //el valor de el tipo de credito lo combierte en dos digitos
-            $numero_pad = str_pad($valor, 2, '0', STR_PAD_LEFT);
-
-            $includCredits = [
-                'description' => 'PLAN 0% ' . $valor . ' CUOTAS',
-                'code' => "$numero_pad" . 'BCR',
-                'installment' => $valor,
-                'terminalNumber' => $datos[0],
-                'merchantCode' => $datos[1],
-            ];
-            //agrega los cretios al objeto
-            array_push($credits, $includCredits);
-        }
-        array_push($ranges, $includeBines);
+        $ranges[] = $range;
     }
+
     fclose($file);
-    $array = [];
-    $include[] = [
+
+    $include = [
         'ranges' => $ranges,
         'credits' => $credits,
     ];
-    $array[] = $include;
 
-    $denco = json_encode($array);
-    echo $denco;
+    $result = [
+        'include' => [$include],
+    ];
+
+    $json_data = json_encode($result, JSON_PRETTY_PRINT);
+    echo $json_data;
 } else {
     echo 'Error al cargar el archivo';
 }
-
 ?>
